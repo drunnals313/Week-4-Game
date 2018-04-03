5f9bc977352209abf676954f64ed1b2425d16b5d
drunnals313 <davidrunnals@gmail.com>
2018-04-02 23:04:51 -0400

    update javascript and parseint

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,43 @@
- var targetNumber = 53;
+ var targetNumber = 53; //need to make random once I can get it to work
 
-    $("#number-to-guess").text(targetNumber);
+$("#number-to-guess").text(targetNumber);
 
-    var crystals = $("#crystals");
+var crystals = $("#crystals");
 
-    var counter = 0;
+var counter = 0;
 
-    var numberOptions = [10, 5, 3, 7];
+var numberOptions = [10, 5, 3, 7]; // need to make random once I can get it to work
 
-    var winSum=0;
+var winSum=0;
 
-    var loseSum=0;
+var loseSum=0;
 
-    var isCalculated = false;
+var isCalculated = false;
 
-    var imageArray = ["chalca.jpg", "Crystal-Quartz.jpg", "fireOpal.jpg", "ocean.jpg"]
+var imageArray = ["chalca.jpg", "Crystal-Quartz.jpg", "fireOpal.jpg", "ocean.jpg"]
 
-    // need to turn this loop into the 4 images and link a number to each
+// need to turn this loop into the 4 images and link a number to each
 
-    for (var i = 0; i< numberOptions.length; i++) {
+for (var i = 0; i< numberOptions.length; i++) {
 
-        // var imageCrystal = $("<img>");
+    // var imageCrystal = $("<img>");
 
 
-        imageArray.addClass("crystal-image");
+    imageArray.addClass("crystal-image");
 
-        //imageArray.attr("src", "assets/immages/fireOpal.jpg");
+    //imageArray.attr("src", "assets/immages/fireOpal.jpg");
 
-        imageArray.attr("data-crystalvalue", numberOptions[i]);
+    imageArray.attr("data-crystalvalue", numberOptions[i]);
 
-        //crystals.append(imageArray);         
-    }
+    //crystals.append(imageArray);         
+}
+
+
 
 crystals.on("click", ".crystal-image", function()  {
     var crystalValue = ($(this).attr("data-crystalvalue"));
+
+    crystalValue = parseInt(crystalValue);
 
     counter += crystalValue;
 
