5e2c4b34f6e97af3e967c1a634d7aebc4428e1bf
drunnals313 <davidrunnals@gmail.com>
2018-04-04 23:38:51 -0400

    updated javascript images to be objects but I am messing things up; might need to start over and then shuffle new backend logic into this front end code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
- var targetNumber = 53; //need to make random once I can get it to work
+var targetNumber = 13; //need to make random once I can get it to work
 
 $("#number-to-guess").text(targetNumber);
 
@@ -6,7 +6,7 @@ var crystals = $("#crystals");
 
 var counter = 0;
 
-var numberOptions = [10, 5, 3, 7]; // need to make random once I can get it to work
+var numberOptions = [10, 5, 3, 7]; // need to make random once I can get it to work   can use math floor random
 
 var winSum=0;
 
@@ -14,20 +14,59 @@ var loseSum=0;
 
 var isCalculated = false;
 
-var imageArray = ["chalca.jpg", "Crystal-Quartz.jpg", "fireOpal.jpg", "ocean.jpg"]
+var Chalca = {
+    name = "Chalca",
+    value = 2
+    //1 + Math.floor(Math.random() * 1), 
+}
+
+var Quartz = {
+    name = "Quartz",
+    value =3
+    //1 + Math.floor(Math.random() * 1),
+}
+
+var Opal = {
+    name = "Opal",
+    value = 5
+    //1 + Math.floor(Math.random() * 1),
+}
+
+var Ocean = {
+    name = "Ocean",
+    value = 8
+    //1 + Math.floor(Math.random() * 1),
+}
+
+
+Chalca.addClass("crystal-image");
+Quartz.addClass("crystal-image");
+Opal.addClass("crystal-image");
+Ocean.addClass("crystal-image");
+
+Chalca.attr("data-crystalvalue", Chalca.value);
+Quartz.attr("data-crystalvalue", Quartz.value);
+Opal.attr("data-crystalvalue", Opal.value);
+Ocean.attr("data-crystalvalue", Ocean.value);
+
+
+
+////var imageArray = {"imageOne": "chalca.jpg", "imageTwo": "Crystal-Quartz.jpg", "imageThree": "fireOpal.jpg", "imageFour": "ocean.jpg" }  
+
+// turn these into objects (box of labeled boxes)  gandalf.age example
 
 // need to turn this loop into the 4 images and link a number to each
 
-for (var i = 0; i< numberOptions.length; i++) {
+////for (var i = 0; i< numberOptions.length; i++) {
 
     // var imageCrystal = $("<img>");
 
 
-    imageArray.addClass("crystal-image");
+    ////imageArray.addClass("crystal-image");
 
     //imageArray.attr("src", "assets/immages/fireOpal.jpg");
 
-    imageArray.attr("data-crystalvalue", numberOptions[i]);
+    ////imageArray.attr("data-crystalvalue", numberOptions[i]);
 
     //crystals.append(imageArray);         
 }
